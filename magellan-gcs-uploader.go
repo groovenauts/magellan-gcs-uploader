08bda3925af9f9d60308aa6e74afab673ce12f4b
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2016-09-01 11:19:20 +0900

    insert uploaded file metadata into BigQuery table.

--- magellan-gcs-uploader.go
+++ magellan-gcs-uploader.go
@@ -8,10 +8,12 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
+	"google.golang.org/cloud/bigquery"
 	"google.golang.org/cloud/storage"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 var (
@@ -45,8 +47,17 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type BigQueryRecord struct {
+	Row map[string]bigquery.Value
+}
+
+func (r *BigQueryRecord) Save() (row map[string]bigquery.Value, insertID string, err error) {
+	return r.Row, "", nil
+}
+
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
+	projectId := appengine.AppID(ctx)
 	response := Response{false, "something wrong."}
 	code := 500
 
@@ -69,22 +80,24 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Check API Token
 	api_key := r.FormValue("key")
+	if apiTokens == nil {
+		apiTokens = strings.Split(mustGetenv(ctx, "API_TOKEN"), ",")
+	}
+	err := verifyApiToken(api_key)
+	if err != nil {
+		response.Message = err.Error()
+		code = 401
+		return
+	}
+
+	// Upload file to GCS
 	filename := r.FormValue("filename")
 	content, err := base64.StdEncoding.DecodeString(r.FormValue("content"))
 	if err != nil {
 		response.Message = "content parameter: invalid base64 encoded."
 		code = 400
-		return
-	}
-
-	if apiTokens == nil {
-		apiTokens = strings.Split(mustGetenv(ctx, "API_TOKEN"), ",")
-	}
-        err = verifyApiToken(api_key)
-	if err != nil {
-		response.Message = err.Error()
-		code = 401
 		return
 	}
 
@@ -109,6 +122,34 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	err = writer.Close()
+	if err != nil {
+		log.Criticalf(ctx, err.Error())
+		code = 500
+		return
+	}
+
+	// Insert metadata to BigQuery
+	bqclient, err := bigquery.NewClient(ctx, projectId)
+	if err != nil {
+		log.Criticalf(ctx, err.Error())
+		code = 500
+		return
+	}
+	table := bqclient.OpenTable(projectId, mustGetenv(ctx, "BIGQUERY_DATASET"), mustGetenv(ctx, "BIGQUERY_TABLE"))
+	uploader := table.NewUploader()
+	record := &BigQueryRecord{}
+	record.Row = make(map[string](bigquery.Value))
+	record.Row["gcs_url"] = "gs://" + bucket_name + "/" + filename
+	record.Row["timestamp"] = time.Now()
+	columns := mustGetenv(ctx, "BIGQUERY_COLUMNS")
+	field_names := strings.Split(columns, ",")
+	for _, fn := range field_names {
+		val := r.FormValue(fn)
+		if val != "" {
+			record.Row[fn] = val
+		}
+	}
+	err = uploader.Put(ctx, record)
 	if err != nil {
 		log.Criticalf(ctx, err.Error())
 		code = 500
